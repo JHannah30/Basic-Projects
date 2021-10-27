46011c854cfde8f2a3e1807e6366d06c5294baa4
Jamie Hannah <jamiehannah1047@gmail.com>
2021-10-27 18:29:35 +0100

    Added elements to StartScreen.js

---
+++ higher-lower/src/StartScreen/StartScreen.js
@@ -0,0 +1,19 @@
+import Logo from '../Images/higher-lower-logo.png';
+import './StartScreen.css';
+
+function StartPage(props){
+    return ( 
+        <div className="hl-start-container">
+            <section className="hl-start-body">
+                <section className="hl-start-item">
+                    <img className="hl-logo" src={Logo} alt="higher or lower game logo" />
+                </section>
+                <section className="hl-start-item">
+                    <button className="hl-start-button" onClick={props.startGame}>Play</button>
+                </section>
+            </section>
+        </div>
+     );
+};
+ 
+export default StartPage;
