14decd68cfdb6f9a2fdc3e1c3e273992657c8455
Jamie Hannah <jamiehannah1047@gmail.com>
2021-10-28 12:16:22 +0100

    Changed styling of start screen to be dark grey. Added a new logo

--- higher-lower/src/App/App.js
+++ higher-lower/src/App/App.js
@@ -56,6 +56,9 @@ function App() {
   // Triggers CSS transitioning to move between pages
   const handleScreenTransition = () => {
     setScreenTransition(true);
+    setTimeout(() => {
+      setScreenTransition(false)
+    }, 1500);
   }
 
 
