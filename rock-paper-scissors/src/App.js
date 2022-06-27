e7da1e999cb349d8d5c1c03d4bb07cd4b1648f4e
Jamie Hannah <jamiehannah1047@gmail.com>
2022-06-27 16:19:33 +0100

    added hover styling and onclick function to each option

--- rock-paper-scissors/src/App.js
+++ rock-paper-scissors/src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  const setWindowVar = () => {
-    window.windowVarSet = true;
+  const userChoosesRock = () => {
+    window.userChoice = "rock";
+    console.log("You chose Rock");
   }
 
+  const userChoosesPaper = () => {
+    window.userChoice = "paper";
+    console.log("You chose Paper");
+  }
 
-
-
+  const userChoosesScissors = () => {
+    window.userChoice = "scissors";
+    console.log("You chose Scissors");
+  }
 
   return (
     <div className="App">
       <main className="App-body centre">
-        <div id="rock" className="clickable-option centre" onClick={setWindowVar}>Rock</div>
-        <div id="paper" className="clickable-option centre" onClick={setWindowVar}>Paper</div>
-        <div id="scissors" className="clickable-option centre" onClick={setWindowVar}>Scissors</div>
+        <div id="rock" className="clickable-option centre" onClick={userChoosesRock}>Rock</div>
+        <div id="paper" className="clickable-option centre" onClick={userChoosesPaper}>Paper</div>
+        <div id="scissors" className="clickable-option centre" onClick={userChoosesScissors}>Scissors</div>
       </main>
     </div>
   );
